7bd251c2345a25194f4dddffda73c5677806ae2f
louisa :3 <louisakavindu@gmail.com>
2026-04-13 14:49:57 +0300

    add favicon and shi

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>pulsewidth | a hack club ysws</title>
+	<link rel="icon" href="/pwfavicon.png" type="image/png" />
+</svelte:head>
+
 {@render children()}
